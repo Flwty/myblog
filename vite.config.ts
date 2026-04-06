8b5a60604864f0f498b201ea484ab8a3e321ebea
Flwty <66114018+Flwty@users.noreply.github.com>
2026-04-06 21:55:07 +0800

    Set base path for Vite configuration
    Added base path configuration for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/myblog/',
+})
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
